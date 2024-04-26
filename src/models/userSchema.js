3672308d200893763c7237f6ad8931169ff6fe11
Woohahang <104793516+Woohahang@users.noreply.github.com>
2024-04-26 20:35:56 +0900

    중복된 임베드 메시지 삭제 기능 추가, setTimestamp 표기 오류 수정

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -1,21 +1,11 @@
 const mongoose = require('mongoose');
 
-function addDefault(value) {
-    if (!value.includes('#')) {
-        value += "#KR1";
-    }
-    return value;
-}
-
 let userSchema = new mongoose.Schema({
     guildId: String,
     userId: String,
     steam: String,
     kakao: String,
-    loL: {
-        type: String,
-        set: addDefault
-    },
+    loL: String,
     updatedAt: { type: Date, default: Date.now }
 });
 
