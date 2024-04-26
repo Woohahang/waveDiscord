61a5c5a5a09c77f8a7c131ba3aec45081e1f1580
Woohahang <104793516+Woohahang@users.noreply.github.com>
2024-04-26 20:28:15 +0900

    중복된 임베드 메시지 삭제 기능 추가, setTimestamp 표기 오류 수정

--- tests/index.test.js
+++ tests/index.test.js
@@ -9,70 +9,117 @@ const userSchema = require('../src/models/userSchema.js');
 // require('../src/index.js');
 connectToDatabase();
 
-client.on('voiceStateUpdate', async (oldState, newState) => {
-    return;
-    if (oldState.channelId !== newState.channelId && newState.channelId !== null) {
-        const memememe = new EmbedBuilder()
-            .setColor('#0099ff')
-            .setTitle('테스트 정보')
+async function removeEmptyFields(userDocument) {
+    // 게임이 더 확장 되면 필드명 추가
+    ['steam', 'kakao', 'loL'].forEach(field => {
+        if (userDocument[field] === '') {
+            userDocument[field] = undefined;
+        }
+    });
 
-        const channel = newState.channel;
-        try {
-            const messages = await channel.messages.fetch(); // 채널의 메시지를 가지고 온다.
-            const embedMessages = messages.filter(message => message.author.id === waveID && message.embeds.length > 0); // 메시지 정보 중에 임베드 길이가 0보다 크면 가지고 온다.
+    await userDocument.save();
+}
 
-            // 가지고 온 메시지를 다 삭제한다.
-            embedMessages.forEach(async embedMessage => {
-                await embedMessage.delete();
-                console.log("이전 임베드 메시지가 성공적으로 삭제되었습니다.");
+async function addDefaultLoL(userDocument) {
+    if (userDocument.loL && !userDocument.loL.includes('#')) {
+        userDocument.loL += '#KR1';
+        await userDocument.save();
+    }
+}
+
+
+function createFields(userDocument) {
+    const fields = [];
+
+    if (userDocument.steam) {
+        // Steam 프로필 주소에 'https://steamcommunity.com/'이 포함되어 있으면, 링크로 표시합니다.
+        // - 포함되어 있지 않으면, 친구 코드를 그대로 표시합니다.
+        const includesSteamCommunity = userDocument.steam.includes("https://steamcommunity.com/");
+        fields.push({
+            name: 'Steam 프로필',
+            value: includesSteamCommunity ? `[스팀 친구 추가](${userDocument.steam})` : userDocument.steam,
+            inline: true
+        });
+    }
+
+    if (userDocument.kakao) {
+        fields.push({
+            name: '카카오 배틀 그라운드',
+            value: `[${userDocument.kakao}](https://dak.gg/pubg/profile/kakao/${userDocument.kakao})`,
+            inline: true
+        });
+    }
+
+    if (userDocument.loL) {
+        fields.push({
+            name: '리그 오브 레전드',
+            value: `[${userDocument.loL}](https://www.op.gg/summoners/kr/${userDocument.loL})`,
+            inline: true
+        });
+    }
+
+    // 게임과 닉네임을 나타내는 정보를 세 개 이상 작성할 경우, 공백을 추가합니다.
+    if (fields.length >= 3) {
+        for (let i = 2; i < fields.length; i += 3) {
+            fields.splice(i, 0, {
+                name: ' ',
+                value: ' ',
+                inline: true
             });
-
-            // 새로운 임베드 메시지를 보냅니다.
-            await channel.send({ embeds: [memememe] });
-            console.log("새로운 임베드 메시지가 성공적으로 전송되었습니다.");
-
-        } catch (error) {
-            console.error(`메시지를 보내는 중에 오류가 발생했습니다. : ${error}`);
         }
     }
-})
-
-function setDefaultValues(userDocument) {  // DB 데이터가 비어 있으면 - 값 적용, 이유 : null 이거나 '' 면 .addFields 의 value가 깨진다.
-
-    userDocument.kakao = userDocument.kakao || '-';
-    userDocument.steam = userDocument.steam || '-';
-    userDocument.loL = userDocument.loL || '-';
-
+    const sads = '';
+    console.log("asdds");
+    return fields;
 }
+
 
 client.on('voiceStateUpdate', async (oldState, newState) => {
     // return;
     if (oldState.channelId !== newState.channelId && newState.channelId !== null) {
-
         const user = newState.member.user;
         const userDocument = await userSchema.findOne({ guildId: newState.guild.id, userId: newState.id });
-        if (!userDocument) return console.log(`사용자의 사용자 프로필을 찾을 수 없습니다. ${newState.id}`);
-        setDefaultValues(userDocument);
         const channel = newState.channel;
 
-        const embedMessage = new EmbedBuilder()
-            .setColor('#0099ff')
-            .setTitle('사용자 정보')
-            .setColor(0x0099FF)
-            .setAuthor({ name: user.globalName, iconURL: user.displayAvatarURL(), url: user.avatarURL() })
-            .addFields(
-                { name: 'Steam 프로필', value: '[스팀 친구 추가](https://steamcommunity.com/profiles/76561198311685982/)', inline: true },
-                { name: '카카오 배틀 그라운드', value: `[${userDocument.kakao}](https://dak.gg/pubg/profile/kakao/${userDocument.kakao})`, inline: true },
-                { name: ' ', value: ' ' },
-                { name: '리그 오브 레전드', value: `[${userDocument.loL}](https://www.op.gg/summoners/kr/${userDocument.loL})`, inline: true },
-                { name: '발로란트', value: `[-](https://dak.gg/valorant/profile/)`, inline: true },
-                { name: '\u200B', value: '\u200B' }
-            )
-            .setTimestamp(userDocument.updatedAt)
-            .setFooter({ text: '━━━━━━━━━━━ update', iconURL: 'https://cdn-icons-png.flaticon.com/512/5052/5052710.png' });
+        if (!userDocument) {
+            console.log("사용자 문서를 찾을 수 없습니다.");
+            return
+        };
 
         try {
-            await channel.send({ embeds: [embedMessage] });
+            await removeEmptyFields(userDocument);
+            await addDefaultLoL(userDocument);
+
+            const fields = createFields(userDocument);
+
+            const embed = new EmbedBuilder()
+                .setColor('#0099ff')
+                .setTitle('사용자 정보')
+                .setColor(0x0099FF)
+                .setAuthor({ name: user.globalName, iconURL: user.displayAvatarURL(), url: user.avatarURL() })
+                .addFields(...fields, { name: '\u200B', value: '\u200B' })
+                .setTimestamp(new Date(userDocument.updatedAt))
+                .setFooter({ text: '━━━━━━━━━━━ update', iconURL: 'https://cdn-icons-png.flaticon.com/512/5052/5052710.png' });
+
+            // 응답 받은 (음성 채널) 의 메시지 10개를 가지고 옵니다.
+            const messages = await channel.messages.fetch({ limit: 10 });
+
+            // wave bot 이 채팅 친 메세지 중에 embed 메세지들을 가지고 옵니다.
+            const filterEmbeds = messages.filter(message => message.author.id === waveID && message.embeds.length > 0);
+
+            // 중복된 임베드를 삭제합니다.
+            filterEmbeds.forEach(message => {
+                message.embeds.forEach(embed => {
+                    if (embed.author.name == user.globalName && embed.author.url == user.avatarURL() && embed.author.iconURL == user.displayAvatarURL()) {
+                        message.delete();
+                        console.log("같은 정보 삭제");
+                    }
+                });
+            });
+
+            // 임베드 전송
+            await channel.send({ embeds: [embed] });
+
         } catch (error) {
             console.error(`메시지를 보내는 중에 오류가 발생했습니다.! : ${error}`);
         }
@@ -80,18 +127,16 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
 
 })
 
+
 client.login(token);
 
 /* 작업 목록
-    1. 테스트 폴더 생성
-    임베드 업데이트 계획 : 하나의 임베드에 채널의 유저 정보 업데이트.
-    값이 없는 필드는 생략.
-    
-    2. 임베드 메시지가 이미 존재한다면 삭제 후 새로운 메시지 전송
-    
-    3. userSchema 수정
-    userSchema.updatedAt 접근 시, 의도와 맞지 않게 현재 시간만 내보내던 점을 마지막 업데이트 시각이 나오도록 수정.
+    1. setTimestamp 표기 오류 수정 ( 현재시간 표기 -> 마지막 업데이트 시간 표기 )
 
-    4. userSchema 기본 값 추가
-    loL 필드에 '#'이 없는 경우 자동으로 '#KR1'을 추가하여 기본 값 설정
+    2. 중복 된 임베드 메시지 삭제 기능 추가
+
+    - 메모
+    1. Collection 객체는 일반적인 forEach 문법을 사용할 수 없습니다. 대신 .forEach() 메서드를 사용하여 반복 작업을 수행해야 합니다.
+    2. discord.js 에서 지원하는 객체는 Collection 문법에 따를 것
+        - ex) 임베드의 개수를 헤아릴 때 embeds.length --> embeds.size
 */
