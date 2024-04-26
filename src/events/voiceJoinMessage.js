7c06e04d0f90a52ad669349b36ac5f22f2b043ba
Woohahang <104793516+Woohahang@users.noreply.github.com>
2024-04-26 20:28:55 +0900

    중복된 임베드 메시지 삭제 기능 추가, setTimestamp 표기 오류 수정

--- src/events/voiceJoinMessage.js
+++ src/events/voiceJoinMessage.js
@@ -1,62 +1,125 @@
 // src/events/voiceJoinMessage.js
 
-function setDefaultValues(userDocument) {  // DB 데이터가 비어 있으면 - 값 적용, 이유 : null 이거나 '' 면 .addFields 의 value가 깨진다.
-
-    userDocument.kakao = userDocument.kakao || '-';
-    userDocument.steam = userDocument.steam || '-';
-    userDocument.loL = userDocument.loL || '-';
-
-}
-
 const { EmbedBuilder } = require('discord.js');
 const userSchema = require('../models/userSchema.js');
+const { waveID } = require('../../../config.json');
+
+async function removeEmptyFields(userDocument) {
+    // 게임이 더 확장 되면 필드명 추가
+    ['steam', 'kakao', 'loL'].forEach(field => {
+        if (userDocument[field] === '') {
+            userDocument[field] = undefined;
+        }
+    });
+
+    await userDocument.save();
+}
+
+async function addDefaultLoL(userDocument) {
+    if (userDocument.loL && !userDocument.loL.includes('#')) {
+        userDocument.loL += '#KR1';
+        await userDocument.save();
+    }
+}
+
+
+function createFields(userDocument) {
+    const fields = [];
+
+    if (userDocument.steam) {
+        // Steam 프로필 주소에 'https://steamcommunity.com/'이 포함되어 있으면, 링크로 표시합니다.
+        // - 포함되어 있지 않으면, 친구 코드를 그대로 표시합니다.
+        const includesSteamCommunity = userDocument.steam.includes("https://steamcommunity.com/");
+        fields.push({
+            name: 'Steam 프로필',
+            value: includesSteamCommunity ? `[스팀 친구 추가](${userDocument.steam})` : userDocument.steam,
+            inline: true
+        });
+    }
+
+    if (userDocument.kakao) {
+        fields.push({
+            name: '카카오 배틀 그라운드',
+            value: `[${userDocument.kakao}](https://dak.gg/pubg/profile/kakao/${userDocument.kakao})`,
+            inline: true
+        });
+    }
+
+    if (userDocument.loL) {
+        fields.push({
+            name: '리그 오브 레전드',
+            value: `[${userDocument.loL}](https://www.op.gg/summoners/kr/${userDocument.loL})`,
+            inline: true
+        });
+    }
+
+    // 게임과 닉네임을 나타내는 정보를 세 개 이상 작성할 경우, 공백을 추가합니다.
+    if (fields.length >= 3) {
+        for (let i = 2; i < fields.length; i += 3) {
+            fields.splice(i, 0, {
+                name: ' ',
+                value: ' ',
+                inline: true
+            });
+        }
+    }
+
+    return fields;
+}
 
 module.exports = async (oldState, newState) => {
-    // 유저 정보를 데이터베이스에서 가져옵니다.
-    const userDocument = await userSchema.findOne({ guildId: newState.guild.id, userId: newState.id });
 
-    // 유저 정보가 없으면 함수를 종료합니다.
-    if (!userDocument) return console.log(`사용자의 사용자 프로필을 찾을 수 없습니다. ${newState.id}`);
+    if (oldState.channelId !== newState.channelId && newState.channelId !== null) {
+        const user = newState.member.user;
+        const userDocument = await userSchema.findOne({ guildId: newState.guild.id, userId: newState.id });
+        const channel = newState.channel;
 
-    // 음성 채널을 가져옵니다.
-    let channel;
-    try {
-        channel = await newState.guild.channels.fetch(newState.channelId);
-    } catch (error) {
-        console.error(`채널을 가져오는 중에 오류가 발생했습니다. : ${error}`);
-        return;
+        if (!userDocument) {
+            console.log("사용자 문서를 찾을 수 없습니다.");
+            return
+        };
+
+        try {
+            await removeEmptyFields(userDocument);
+            await addDefaultLoL(userDocument);
+
+            const fields = createFields(userDocument);
+
+            const embed = new EmbedBuilder()
+                .setColor('#0099ff')
+                .setTitle('사용자 정보')
+                .setColor(0x0099FF)
+                .setAuthor({ name: user.globalName, iconURL: user.displayAvatarURL(), url: user.avatarURL() })
+                .addFields(...fields, { name: '\u200B', value: '\u200B' })
+                .setTimestamp(new Date(userDocument.updatedAt))
+                .setFooter({ text: '━━━━━━━━━━━ update', iconURL: 'https://cdn-icons-png.flaticon.com/512/5052/5052710.png' });
+
+            // 응답 받은 (음성 채널) 의 메시지 10개를 가지고 옵니다.
+            const messages = await channel.messages.fetch({ limit: 10 });
+
+            // wave bot 이 채팅 친 메세지 중에 embed 메세지들을 가지고 옵니다.
+            const filterEmbeds = messages.filter(message => message.author.id === waveID && message.embeds.length > 0);
+
+            // 중복된 임베드를 삭제합니다.
+            filterEmbeds.forEach(message => {
+                message.embeds.forEach(embed => {
+                    if (embed.author.name == user.globalName && embed.author.url == user.avatarURL() && embed.author.iconURL == user.displayAvatarURL()) {
+                        if (message) {
+                            message.delete();
+                            console.log("중복 임베드 삭제");
+                        } else {
+                            console.log("삭제할 메시지를 찾을 수 없습니다. / 간혹 API 문제");
+                        }
+                    }
+                });
+            });
+
+            // 임베드 전송
+            await channel.send({ embeds: [embed] });
+
+        } catch (error) {
+            console.error(`메시지를 보내는 중에 오류가 발생했습니다.! : ${error}`);
+        }
     }
 
-    // 채널이 없으면 함수를 종료합니다.
-    if (!channel) return console.log(`사용자의 채널을 찾을 수 없습니다 ${newState.id}`);
-
-    setDefaultValues(userDocument);
-
-    const user = newState.member.user;
-
-    const embedMessage = new EmbedBuilder()
-        .setColor('#0099ff')
-        .setTitle('사용자 정보')
-        .setColor(0x0099FF)
-        .setAuthor({ name: user.globalName, iconURL: user.displayAvatarURL(), url: user.avatarURL() })
-        .addFields(
-            { name: 'Steam 프로필', value: '[스팀 친구 추가](https://steamcommunity.com/profiles/76561198311685982/)', inline: true },
-            { name: '카카오 배틀 그라운드', value: `[${userDocument.kakao}](https://dak.gg/pubg/profile/kakao/${userDocument.kakao})`, inline: true },
-            { name: ' ', value: ' ' },
-            { name: '리그 오브 레전드', value: `[${userDocument.loL}](https://www.op.gg/summoners/kr/${userDocument.loL})`, inline: true },
-            { name: '발로란트', value: `[-](https://dak.gg/valorant/profile/)`, inline: true },
-            { name: '\u200B', value: '\u200B' }
-        )
-        .setTimestamp(userSchema.updatedAt)
-        .setFooter({ text: '━━━━━━━━━━━ update', iconURL: 'https://cdn-icons-png.flaticon.com/512/5052/5052710.png' });
-
-    // 채널에 메시지를 전송합니다.
-
-    try {
-        await channel.send({ embeds: [embedMessage] });
-    } catch (error) {
-        console.error(`메시지를 보내는 중에 오류가 발생했습니다. : ${error}`);
-    }
-
-
-};
+}
