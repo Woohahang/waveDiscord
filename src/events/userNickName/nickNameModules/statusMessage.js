1bdcff27fce079729c538fce135adf8971d2593d
Woohahang <glass1726@naver.com>
2024-06-10 22:31:36 +0900

    닉네임 저장 상태 메세지

--- src/events/userNickName/nickNameModules/statusMessage.js
+++ src/events/userNickName/nickNameModules/statusMessage.js
@@ -1,25 +1,44 @@
 // statusMessage.js
 
+const saveSuccess =
+    '\n' + `## 닉네임 등록 완료` +
+    '\n' + '> * 음성 채널 입장 후 채팅을 열어보세요 !' +
+    '\n' + '> * **Wave** 가 사용자 정보를 보여줍니다 !';
+
+const nicknameLimitExceeded =
+    '\n' + `## 닉네임 등록 실패` +
+    '\n' + '> * 해당 게임은 저장할 수 있는 닉네임 개수를 초과했습니다.' +
+    '\n' + '> * 문제가 지속되면 **Wave** 디스코드 채널로 문의해 주세요.';
+
+const nicknameDuplicate =
+    '\n' + `## 닉네임 등록 실패` +
+    '\n' + '> * 중복 된 닉네임이 있습니다.' +
+    '\n' + '> * 문제가 지속되면 **Wave** 디스코드 채널로 문의해 주세요.';
+
 function statusMessage(status) {
-    let message;
-    switch (status) {
-        case 'nicknameDuplicate':
-            message = '중복 된 닉네임이 있습니다.';
-            break;
+    try {
+        let message;
+        switch (status) {
+            case 'nicknameDuplicate':
+                message = nicknameDuplicate;
+                break;
 
-        case 'nicknameLimitExceeded':
-            message = '해당 게임은 저장할 수 있는 닉네임 개수를 초과했습니다.';
-            break;
+            case 'nicknameLimitExceeded':
+                message = nicknameLimitExceeded;
+                break;
 
-        case 'saveSuccess':
-            message = '닉네임 등록 완료 !';
-            break;
+            case 'saveSuccess':
+                message = saveSuccess;
+                break;
 
-        default:
-            message = '알 수 없는 오류로 인해 처리하지 못 했습니다.';
-            break;
+            default:
+                message = '알 수 없는 오류로 인해 처리하지 못 했습니다.';
+                break;
+        };
+        return message;
+    } catch (error) {
+        console.error('statusMessage.js 예외 : ', error);
     };
-    return message;
 };
 
 module.exports = statusMessage;
