bce61f94f5276541a0228ddac3b649d3803e88ad
Woohahang <glass1726@naver.com>
2024-06-27 20:54:09 +0900

    리팩터링

--- src/events/guildCreate/setupChannel/mainChannel/modules/sendMainMessage.js
+++ src/events/guildCreate/setupChannel/mainChannel/modules/sendMainMessage.js
@@ -1,14 +1,17 @@
 const gameMenu = require('../../../../../module/games/gameMenu');
 const waveButton = require('../modules/waveButton');
 
+const contentMessage =
+    '## :star: Wave 메인 명령어' + '\n' +
+    '## /닉네임등록  /닉네임삭제';
+
 // Wave 메인 채널 메세지 전송 및 메세지 id 반환
 async function sendMainMessage(channel, guildData) {
     try {
-        await channel.send({
-            content: "## :star: Wave 메인 명령어\n## /닉네임등록  /닉네임삭제",
-            components: [gameMenu(guildData)],
-        });
+        // 첫 번째 메세지를 보냅니다.
+        await channel.send({ content: contentMessage, components: [gameMenu(guildData)] });
 
+        // 두 번째 메세지를 보냅니다.
         await channel.send({ components: [waveButton()] });
 
     } catch (error) {
