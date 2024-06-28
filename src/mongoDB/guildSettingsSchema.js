3b539afb016d503ac1247f1b910a582a5d05a760
Woohahang <glass1726@naver.com>
2024-06-28 19:04:25 +0900

    길드 필드 기본 값 변경

--- src/mongoDB/guildSettingsSchema.js
+++ src/mongoDB/guildSettingsSchema.js
@@ -17,14 +17,14 @@ let guildSettingsSchema = new mongoose.Schema({
 
 
     // 길드에서 다루는 게임
-    steam: { type: Boolean, default: true },
-    leagueOfLegends: { type: Boolean, default: true },
-    teamfightTactics: { type: Boolean, default: true },
-    valorant: { type: Boolean, default: true },
-    steamBattleGround: { type: Boolean, default: true },
-    kakaoBattleGround: { type: Boolean, default: true },
-    blizzard: { type: Boolean, default: true },
-    overWatchTwo: { type: Boolean, default: true },
+    steam: { type: Boolean, default: false },
+    leagueOfLegends: { type: Boolean, default: false },
+    teamfightTactics: { type: Boolean, default: false },
+    valorant: { type: Boolean, default: false },
+    steamBattleGround: { type: Boolean, default: false },
+    kakaoBattleGround: { type: Boolean, default: false },
+    blizzard: { type: Boolean, default: false },
+    overWatchTwo: { type: Boolean, default: false },
 
 });
 
