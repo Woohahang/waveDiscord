5eae14f9f33b4cfd0f262cffd144bd2131c95d45
Woohahang <glass1726@naver.com>
2024-07-12 04:54:10 +0900

    주석 리팩터링

--- src/events/userChannel/modules/deleteWaveMessages.js
+++ src/events/userChannel/modules/deleteWaveMessages.js
@@ -1,18 +1,19 @@
 const { clientId } = require('../../../../../config.json');
 
 /**
- * Wave 메세지를 모두 제거합니다.
+ * 채널에서 Wave 봇이 보낸 메시지를 모두 제거합니다.
  * 
-*/
+ * @param {TextChannel|DMChannel} channel - 메시지를 삭제할 디스코드 채널 객체.
+ */
 async function deleteWaveMessages(channel) {
     try {
         // 최근 30개의 메시지를 가져옵니다.
         const messages = await channel.messages.fetch({ limit: 30 });
 
-        // Wave 봇이 보낸 메시지를 필터링합니다.
+        // 최근 30개의 메시지 중에서 Wave 봇이 보낸 메시지를 필터링합니다.
         const waveMessages = messages.filter(message => message.author.id === clientId);
 
-        // Wave 봇이 보낸 메시지를 삭제합니다.
+        // 필터링된 Wave 봇의 메시지를 모두 삭제합니다.
         await Promise.all(waveMessages.map(message => message.delete()));
     } catch (error) {
         console.error('deleteWaveMessages.js 예외 : ', error);
