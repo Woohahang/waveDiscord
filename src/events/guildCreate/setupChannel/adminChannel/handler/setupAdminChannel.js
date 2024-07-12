39ef566975e7aed195e6caaf7deb55da77f1bf8f
Woohahang <glass1726@naver.com>
2024-07-12 18:01:42 +0900

    셋업 채널 리팩터링

--- src/events/guildCreate/setupChannel/adminChannel/handler/setupAdminChannel.js
+++ src/events/guildCreate/setupChannel/adminChannel/handler/setupAdminChannel.js
@@ -1,6 +1,7 @@
 const GuildSettings = require('../../../../../services/GuildSettings');
 const createAdminChannel = require('../modules/createAdminChannel');
 const sendAdminMessage = require('../modules/sendAdminMessage');
+const deleteOldMessages = require('../../../update/updateModule/deleteOldMessages');
 
 /**
  * 길드 설정을 로드하거나 생성하고, 관리자 채널을 설정합니다.
@@ -12,7 +13,7 @@ module.exports = async (guild) => {
     try {
         // 길드 인스턴스 생성 및 길드 데이터를 불러옵니다.
         const guildSettings = new GuildSettings(guild.id);
-        const guildData = await guildSettings.loadOrCreate()
+        const guildData = await guildSettings.loadOrCreate();
 
         // 관리자 채널을 가져오려고 시도합니다.
         let channel = await guild.channels.fetch(guildData.adminChannelId)
@@ -29,6 +30,9 @@ module.exports = async (guild) => {
 
         // 관리자 메시지를 새 채널에 전송합니다.
         await sendAdminMessage(channel, guildData);
+
+        // 최근 메세지 두개를 제외한 나머지 메세지를 삭제합니다.
+        await deleteOldMessages(channel);
 
     } catch (error) {
         console.error('setupAdminChannel.js 예외 : ', error);
--- src/events/guildCreate/setupChannel/mainChannel/handler/setupMainChannel.js
+++ src/events/guildCreate/setupChannel/mainChannel/handler/setupMainChannel.js
@@ -1,19 +1,38 @@
 const GuildSettings = require('../../../../../services/GuildSettings');
 const createMainChannel = require('../modules/createMainChannel');
 const sendMainMessage = require('../modules/sendMainMessage');
+const deleteOldMessages = require('../../../update/updateModule/deleteOldMessages');
 
+/**
+ * 길드 설정을 로드하거나 생성하고, 메인 채널을 설정합니다.
+ * 메인 채널이 존재하지 않으면 새로 생성하고, 메시지를 전송합니다.
+ * 
+ * @param {Guild} guild - 설정을 적용할 Discord 길드 객체
+ */
 module.exports = async (guild) => {
     try {
-        // Wave 메인 채널을 생성합니다.
-        const channel = await createMainChannel(guild);
-
-        // 길드 인스턴스 생성 및 채널 Id 저장
+        // 길드 인스턴스 생성 및 길드 데이터를 불러옵니다.
         const guildSettings = new GuildSettings(guild.id);
         const guildData = await guildSettings.loadOrCreate();
+
+        // 메인 채널을 가져오려고 시도합니다.
+        let channel = await guild.channels.fetch(guildData.mainChannelId)
+            .then(channel => channel)
+            .catch(error => null);
+
+        // 채널이 존재하지 않으면 새로 생성합니다.
+        if (!channel) {
+            channel = await createMainChannel(guild);
+        };
+
+        // 새 채널 ID를 길드 설정에 저장합니다.
         await guildSettings.saveChannelId('mainChannel', channel.id);
 
         // Wave 메인 채널에 메세지를 보냅니다.
         await sendMainMessage(channel, guildData);
+
+        // 최근 메세지 두개를 제외한 나머지 메세지를 삭제합니다.
+        await deleteOldMessages(channel);
 
     } catch (error) {
         console.error('setupMainChannel.js 예외 : ', error);
