89f6da89c5dbac01d5d382a80fbe827e09b307b1
hyeonWoo <glass1726@naver.com>
2025-04-17 21:23:37 +0900

    refactor: emojiId 함수와 상수 객체를 분리하고 파일 네이밍 개선

---
+++ src/constants/emojiIds.js
@@ -0,0 +1,15 @@
+const emojiIds = {
+    leagueOfLegends: '1340150566801637496',
+    overWatchTwo: '1340150585655165069',
+    valorant: '1340150638167720057',
+    steamBattleGround: '1340150616495755294',
+    steam: '1340150607302103183',
+    rainbowSix: '1340150594220064809',
+    lostArk: '1340150575756480523',
+    kakaoBattleGround: '1340150557515579543',
+    blizzard: '1340150546958389319',
+    apex: '1340150527169659023',
+    teamfightTactics: '1277689044607963368',
+};
+
+module.exports = emojiIds;
--- src/events/voiceChannelEmbed/module/emojiId.js
+++ src/events/voiceChannelEmbed/module/emojiId.js
@@ -1,47 +1,13 @@
+const emojiIds = require('../../../constants/emojiIds');
+
 /**
- * 주어진 게임 이름에 해당하는 이모지 ID를 반환하는 함수입니다.
- * 
- * @param {string} gameName - 이모지 ID를 가져올 게임의 이름.
- * @returns {string|null} - 게임 이름에 해당하는 이모지 ID를 반환합니다.
-*/
+ * 게임 이름에 해당하는 이모지 ID를 반환합니다.
+ *
+ * @param {string} gameName - 게임 키 값
+ * @returns {string|null} 이모지 ID 또는 null
+ */
 function emojiId(gameName) {
-    switch (gameName) {
-        case 'leagueOfLegends':
-            return '1340150566801637496';
-
-        case 'overWatchTwo':
-            return '1340150585655165069';
-
-        case 'valorant':
-            return '1340150638167720057';
-
-        case 'steamBattleGround':
-            return '1340150616495755294';
-
-        case 'steam':
-            return '1340150607302103183';
-
-        case 'rainbowSix':
-            return '1340150594220064809';
-
-        case 'lostArk':
-            return '1340150575756480523';
-
-        case 'kakaoBattleGround':
-            return '1340150557515579543';
-
-        case 'blizzard':
-            return '1340150546958389319';
-
-        case 'apex':
-            return '1340150527169659023';
-
-        case 'teamfightTactics':
-            return '1277689044607963368';
-
-        default:
-            return null;
-    };
-};
+    return emojiIds[gameName] || null;
+}
 
 module.exports = emojiId;
