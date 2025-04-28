8795f71ad1a94d0dfc856727b25dc5a35a54010c
hyeonWoo <glass1726@naver.com>
2025-04-28 23:29:36 +0900

    Doc: 주석 추가

--- src/shared/utils/validateRiotTagFormat.js
+++ src/shared/utils/validateRiotTagFormat.js
@@ -1,12 +1,20 @@
 const { InvalidRiotTagFormatError } = require('../../utils/errors/errorType');
+
+/**
+ * 라이엇 태그 포맷을 검사하기 위한 정규 표현식
+ * 
+ * 형식: '닉네임#태그' 형태로, 닉네임은 최대 16자, 태그는 최대 5자까지 가능
+ */
 const RIOT_TAG_REGEX = /^[\p{L}0-9 ]{1,16}#[\p{L}0-9]{1,5}$/u;
 
 /**
  * 닉네임이 라이엇 태그 포맷에 맞는지 검사
+ * 
  * @param {string} nickname
  * @throws {Error} INVALID_RIOT_TAG_FORMAT
  */
 function validateRiotTagFormat(nickname) {
+
     if (!RIOT_TAG_REGEX.test(nickname)) {
         throw new InvalidRiotTagFormatError();
     }
--- src/utils/errors/sendErrorMessage.js
+++ src/utils/errors/sendErrorMessage.js
@@ -1,5 +1,11 @@
 const errorMessages = require('./errorMessages');
 
+/**
+ * 주어진 에러 코드에 해당하는 에러 메시지를 사용자에게 전송하는 함수
+ * 
+ * @param {string} code - 전송할 에러 메시지의 코드 (예: 'INVALID_PROFILE_LINK', 'USER_PROFILE_NOT_FOUND' 등)
+ * @param {Object} interaction - Discord interaction 객체, 에러 메시지를 보낼 채팅 인터페이스
+ */
 async function sendErrorMessage(code, interaction) {
     try {
         await interaction.editReply({ content: errorMessages[code], ephemeral: true });
