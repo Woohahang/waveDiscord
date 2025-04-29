a838dabc8cab65cf54dcd5ac60216eddd56b0a3a
hyeonWoo <glass1726@naver.com>
2025-04-29 09:07:31 +0900

    Chore: Jest 설정에 모듈 경로 별칭 추가

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    moduleNameMapper: {
+        '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+        '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+        '^@services/(.*)$': '<rootDir>/src/services/$1',
+        '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+    },
+};
