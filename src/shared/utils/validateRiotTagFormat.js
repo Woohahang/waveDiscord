ae7efb194ddd3acaccf04d158b43c8e708dc4309
hyeonWoo <glass1726@naver.com>
2025-05-03 20:20:02 +0900

    Fix: 라이엇 태그 정규식에서 태그 부분 공백 허용

--- src/shared/utils/validateRiotTagFormat.js
+++ src/shared/utils/validateRiotTagFormat.js
@@ -5,7 +5,7 @@ const { InvalidRiotTagFormatError } = require('../../utils/errors/errorType');
  * 
  * 형식: '닉네임#태그' 형태로, 닉네임은 최대 16자, 태그는 최대 5자까지 가능
  */
-const RIOT_TAG_REGEX = /^[\p{L}0-9 ]{1,16}#[\p{L}0-9]{1,5}$/u;
+const RIOT_TAG_REGEX = /^[\p{L}0-9 ]{1,16}#[\p{L}0-9 ]{1,5}$/u;
 
 /**
  * 닉네임이 라이엇 태그 포맷에 맞는지 검사
