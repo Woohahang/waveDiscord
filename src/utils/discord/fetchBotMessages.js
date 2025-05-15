c2ea17fc9320931881bec9b415eb78b32c40411b
KimHyeonWoo <glass1726@naver.com>
2025-05-16 01:11:54 +0900

    refactor: Discord 메시지 필터링 로직 분리 및 유틸 함수화

--- src/utils/discord/fetchBotMessages.js
+++ src/utils/discord/fetchBotMessages.js
@@ -1,4 +1,5 @@
 const logger = require('@utils/logger');
+const botInfo = require('@utils/botInfo');
 
 /**
  * 주어진 채널에서 Wave 봇이 보낸 메시지를 가져옵니다.
@@ -8,14 +9,15 @@ const logger = require('@utils/logger');
  * @param {number} limit - 가져올 메시지 수 (기본값: 30)
  * @returns {Promise<import('discord.js').Collection<string, import('discord.js').Message>>} - 봇 메시지 컬렉션
  */
-async function fetchBotMessages(channel, botId, limit = 30) {
+async function fetchBotMessages(channel, limit = 30) {
     try {
         const messages = await channel.messages.fetch({ limit });
-        return messages.filter(msg => msg.author.id === botId);
+        return messages.filter(msg => msg.author.id === botInfo.get().botId);
     } catch (error) {
         logger.error('[fetchBotMessages] Wave 봇이 보낸 메시지를 가져오는 중 오류', {
             stack: error.stack
         })
+        throw error;
     }
 }
 
--- src/utils/discord/getWaveBotMessagesFromCache.js
+++ src/utils/discord/getWaveBotMessagesFromCache.js
@@ -1,5 +1,11 @@
 const botInfo = require('@utils/botInfo');
 
+/**
+ * 해당 채널의 메시지 캐시에서 Wave 봇이 보낸 메시지만 필터링합니다.
+ *
+ * @param {TextChannel|VoiceChannel} channel - 디스코드 채널 객체
+ * @returns {Collection<Snowflake, Message>} Wave 봇이 보낸 메시지 컬렉션
+ */
 function getWaveBotMessagesFromCache(channel) {
     const messages = channel.messages.cache;
     return messages.filter(msg => msg.author.id === botInfo.get().botId);
