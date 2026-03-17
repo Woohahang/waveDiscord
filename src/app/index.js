fc1346c59383a783a0603094f4df3c7c516317d8
KimHyeonWoo <glass1726@naver.com>
2026-03-17 16:44:43 +0900

    refactor: 앱 시작 흐름을 main 함수 중심으로 정리

--- src/app/index.js
+++ src/app/index.js
@@ -18,38 +18,39 @@ async function main() {
     const dependencies = createDependencies();
 
     registerEvents(client, dependencies);
+
+    await client.login(token);
 }
 
 main();
 
-const commandsRootPath = path.join(__dirname, '../commands');
-const commandFolders = fs.readdirSync(commandsRootPath);
-client.commands = new Collection();
+// const commandsRootPath = path.join(__dirname, '../commands');
+// const commandFolders = fs.readdirSync(commandsRootPath);
+// client.commands = new Collection();
 
-commandFolders.forEach(folder => {
-    const folderPath = path.join(commandsRootPath, folder);
-    const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.js'));
+// commandFolders.forEach(folder => {
+//     const folderPath = path.join(commandsRootPath, folder);
+//     const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.js'));
 
-    commandFiles.forEach(file => {
-        const filePath = path.join(folderPath, file);
-        const command = require(filePath);
+//     commandFiles.forEach(file => {
+//         const filePath = path.join(folderPath, file);
+//         const command = require(filePath);
 
-        if ('data' in command && 'execute' in command)
-            client.commands.set(command.data.name, command);
-        else
-            logger.warn(`[index] ${filePath} 명령어 파일의 필수 속성 "data" 또는 "execute"가 없습니다.`);
-    })
-})
+//         if ('data' in command && 'execute' in command)
+//             client.commands.set(command.data.name, command);
+//         else
+//             logger.warn(`[index] ${filePath} 명령어 파일의 필수 속성 "data" 또는 "execute"가 없습니다.`);
+//     })
+// })
 
-const eventsPath = path.join(__dirname, '../events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+// const eventsPath = path.join(__dirname, '../events');
+// const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-eventFiles.forEach(file => {
-    const event = require(`@events/${file}`);
-    if (event.once)
-        client.once(event.name, (...args) => event.execute(...args));
-    else
-        client.on(event.name, (...args) => event.execute(...args));
-});
+// eventFiles.forEach(file => {
+//     const event = require(`@events/${file}`);
+//     if (event.once)
+//         client.once(event.name, (...args) => event.execute(...args));
+//     else
+//         client.on(event.name, (...args) => event.execute(...args));
+// });
 
-client.login(token);
